eeeec7d4e28dcba2b9966ccc83b43b58744cfbfa
thejonymyster <thejonymyster@gmail.com>
2021-03-12 04:44:33 -0500

    css

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-var humanity = "A human"
+// global.humanity = "A human";
 
 function Home(props) {
     return (
         <>
             <h3>Welcome to the cool cat website</h3>
-            <p>You are: {humanity}</p>
+            {/* <p>You are: {humanity}</p> */}
         </>
     );
 }
 
 export default Home;
-export {humanity};
+// export {humanity};
